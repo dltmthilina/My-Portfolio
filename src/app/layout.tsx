d62f6c42f4f18755275b0491ffc98a60089a0b66
dltmthilina <dltmthilinamaduranga@gmail.com>
2025-05-23 11:00:40 +0530

    update root layout with route transistion animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,39 @@
+"use client";
+
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Thilina Disanayaka Portfolio",
-  description: "Fullstack Software Engineer Portfolio",
-};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+
   return (
     <html lang="en">
       <body
         className={`${inter.className} bg-white dark:bg-gray-900 transition-colors`}
       >
-        {children}
+        <Navbar />
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={pathname}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.4 }}
+            className="pt-20"
+          >
+            {children}
+          </motion.div>
+        </AnimatePresence>
       </body>
     </html>
   );
