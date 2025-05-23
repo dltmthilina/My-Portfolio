0e5bd3849473b33e74dde5911d44cd79c6742a55
dltmthilina <dltmthilinamaduranga@gmail.com>
2025-05-23 11:01:34 +0530

    update home page name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import Hero3D from "@/sections/Hero3D";
 import ThemeToggle from "@/components/ThemeToggle";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className="relative min-h-screen overflow-hidden bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
       <ThemeToggle />
