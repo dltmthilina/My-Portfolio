3b82c56f5c849c9ec349003d125f565f2a78812e
dltmthilina <dltmthilinamaduranga@gmail.com>
2025-05-31 22:24:17 +0530

    import all section on root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,19 @@
 
 import Hero3D from "@/sections/Hero3D";
 import ThemeToggle from "@/components/ThemeToggle";
+import ProjectsSection from "@/sections/Projects";
+import About from "@/sections/About";
+import ExperienceSection from "@/sections/Experience";
+import ContactSection from "@/sections/Contact";
 
 export default function HomePage() {
   return (
-    <main className="relative min-h-screen overflow-hidden bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
-      <ThemeToggle />
+    <main className="">
       <Hero3D />
+      <About />
+      <ProjectsSection />
+      <ExperienceSection />
+      <ContactSection />
     </main>
   );
 }
